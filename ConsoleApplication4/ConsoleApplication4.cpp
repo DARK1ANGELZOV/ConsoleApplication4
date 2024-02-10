51758b76f444fb054de828304b29a07e35ff6c6f
Dark_Angel <Dark_Angel@DESKTOP-15E22GB>
2024-02-10 21:24:22 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int binaryToDecimal(int binaryNumber) {
+    int decimalNumber = 0, base = 0;
+    while (binaryNumber != 0) {
+        int remainder = binaryNumber % 10;
+        decimalNumber += remainder * pow(2, base);
+        binaryNumber /= 10;
+        base++;
+    }
+    return decimalNumber;
+}
+
+int main() {
+    int binaryNumber;
+    cout << "Введите число в двоичной системе: ";
+    cin >> binaryNumber;
+    int decimalNumber = binaryToDecimal(binaryNumber);
+    cout << "Число в десятичной системе: " << decimalNumber << endl;
+    return 0;
+}
